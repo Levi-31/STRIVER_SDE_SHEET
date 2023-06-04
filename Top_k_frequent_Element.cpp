3debdfc396bec4cdee666b82dc4d77ba8f85a1f8
Levi-31 <shuklasarthak045@gmail.com>
2023-06-04 16:01:42 +0530

    first commit

---
+++ Top_k_frequent_Element.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+
+typedef pair<int,int> pi;
+vector<int> KMostFrequent(int n, int k, vector<int> &nums)
+{
+     unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        
+       priority_queue<pi , vector<pi>,greater<pi>> q;
+       for(auto x:m){
+           q.push({x.second , x.first});
+           if(q.size()>k)
+               q.pop();
+       }
+        // cout<<m[nums[0]]<<endl;
+        
+        vector<int> ans;
+        
+        while(q.size()){
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+
+
+}
