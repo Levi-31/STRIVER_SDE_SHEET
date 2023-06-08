489fde275799b902ecd301c59c0488b4baff6824
Levi-31 <shuklasarthak045@gmail.com>
2023-06-08 22:55:41 +0530

    8TH JUNE

---
+++ INORDER.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+
+void solve(TreeNode *root, vector<int>&ans){
+    if(root==NULL) return ;
+    solve(root->left,ans);
+    ans.push_back(root->data);
+    solve(root->right,ans);
+}
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    solve(root,ans);
+
+    return ans;
+}
