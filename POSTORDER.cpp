a14dd824f9e0707e521ce8060ff0875c06b6e153
Levi-31 <shuklasarthak045@gmail.com>
2023-06-09 21:59:01 +0530

    9th June

---
+++ POSTORDER.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+
+void solve(TreeNode *root, vector<int>&ans){
+    if(!root) return ;
+    solve(root->left,ans);
+    solve(root->right,ans);
+    ans.push_back(root->data);
+}
+vector<int> getPostOrderTraversal(TreeNode *root)
+{
+   vector<int>ans;
+   solve(root,ans);
+   return ans;
+}
