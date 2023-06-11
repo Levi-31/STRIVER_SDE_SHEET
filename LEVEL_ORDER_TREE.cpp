c2bd5a24517582bf1f2929e9358129d93d2b201f
Levi-31 <shuklasarthak045@gmail.com>
2023-06-11 15:04:21 +0530

    11th JUNE

---
+++ LEVEL_ORDER_TREE.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+    template <typename T>
+    class BinaryTreeNode {
+       public:
+        T val;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+vector<int> getLevelOrder(BinaryTreeNode<int> *root)
+{
+    if(root==NULL) return {};
+    vector<int> ans;
+    queue<BinaryTreeNode<int> *>q;
+    q.push(root);
+
+    while(!q.empty()){
+        BinaryTreeNode<int> * temp=q.front();
+        q.pop();
+        
+        if(temp->left !=NULL)
+        q.push(temp->left);
+        if(temp->right !=NULL)
+        q.push(temp->right);
+        ans.push_back(temp->val);
+    }
+
+
+    return ans;
+}
