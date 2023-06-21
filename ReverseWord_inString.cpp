2f52f5f1fed12aba7bbbf65fa7ee966ad58fbbf1
Levi-31 <shuklasarthak045@gmail.com>
2023-06-21 14:42:38 +0530

    21st June

---
+++ ReverseWord_inString.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseString(string &str){
+	  stringstream ss(str);
+    stack<string> st;
+    string word;
+    while(ss>>word){
+        st.push(word);
+    }
+    string ans="";
+    while(!st.empty()){
+        string top=st.top();
+        st.pop();
+        ans+=top;
+        ans+=' ';
+    }
+    return ans;
+}
+
