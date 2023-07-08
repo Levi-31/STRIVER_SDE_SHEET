7801992c39c034d6a1adbac1057d03b6e2b27821
Levi-31 <shuklasarthak045@gmail.com>
2023-07-08 15:09:25 +0530

    8th July

---
+++ Sorted_array_toBST.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+        
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+
+TreeNode<int>* solve(vector<int>&arr, int l, int h){
+    if(l>h) return NULL;
+
+    int mid=l+(h-l)/2;
+
+    TreeNode<int>* root= new TreeNode<int>(arr[mid]);
+
+    root->left = solve(arr,l,mid-1);
+    root->right= solve(arr,mid+1,h);
+
+    return root;
+}
+
+TreeNode<int>* sortedArrToBST(vector<int> &arr, int n)
+{
+    return solve(arr,0,n-1);
+}
